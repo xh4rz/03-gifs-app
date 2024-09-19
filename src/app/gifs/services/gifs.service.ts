279a6518c1fd3caa4f114ca0832cb8ec531c0b7e
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-09-19 01:16:25 -0500

    92. GifsService

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class GifsService {
+  private _tagsHistory: string[] = [];
+
+  constructor() {}
+
+  get tagsHistory() {
+    return [...this._tagsHistory];
+  }
+
+  searchTag(tag: string): void {
+    this._tagsHistory.unshift(tag);
+
+    console.log(this._tagsHistory);
+  }
+}
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../../gifs/services/gifs.service';
 
 @Component({
   selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  constructor(private gifsService: GifsService) {}
+
+  get tags() {
+    return this.gifsService.tagsHistory;
+  }
+}
