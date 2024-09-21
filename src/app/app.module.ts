5ac4bd32a5fec009665fb4735f144a4486e15ea3
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-09-21 15:14:56 -0500

    95. Realizar una petición HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, GifsModule, SharedModule],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
@@ -6,7 +7,9 @@ export class GifsService {
 
   private apiKey: string = 'NJsKfG5FuiSYpJyXdohWEgK6GEZ9NXG0';
 
-  constructor() {}
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+  constructor(private http: HttpClient) {}
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -28,5 +31,14 @@ export class GifsService {
     if (tag.length === 0) return;
 
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+
+    this.http
+      .get(`${this.serviceUrl}/search`, { params })
+      .subscribe((resp) => console.log(resp));
   }
 }
