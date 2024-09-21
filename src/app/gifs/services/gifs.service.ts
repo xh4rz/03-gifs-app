83200b8edcf3d3846f4d59f6e7fc61f973b2db6c
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-09-21 14:39:41 -0500

    93. Validaciones al servicio

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -10,9 +10,21 @@ export class GifsService {
     return [...this._tagsHistory];
   }
 
-  searchTag(tag: string): void {
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
+    }
+
     this._tagsHistory.unshift(tag);
 
-    console.log(this._tagsHistory);
+    this._tagsHistory = this.tagsHistory.splice(0, 10);
+  }
+
+  searchTag(tag: string): void {
+    if (tag.length === 0) return;
+
+    this.organizeHistory(tag);
   }
 }
