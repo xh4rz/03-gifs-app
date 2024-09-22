b32a0fb6b5841d5ebee6351bcc6006b363098119
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-09-22 02:34:22 -0500

    100. LocalStorage - Persistencia local

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -28,6 +28,12 @@ export class GifsService {
     this._tagsHistory.unshift(tag);
 
     this._tagsHistory = this.tagsHistory.splice(0, 10);
+
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
   }
 
   searchTag(tag: string): void {
