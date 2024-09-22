c537759a3887b50270e7ea5da03e780cc12937b5
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-09-22 01:59:34 -0500

    97. Mostrar los Gifs en pantalla

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -44,7 +44,6 @@ export class GifsService {
       .get<SearchResponse>(`${this.serviceUrl}/search`, { params })
       .subscribe((resp) => {
         this.gifList = resp.data;
-        console.log({ gifs: resp.data });
       });
   }
 }
